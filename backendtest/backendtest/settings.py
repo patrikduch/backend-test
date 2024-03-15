d2680318f0f3cfc3eb4472e2fd3fde3abc6949a6
patrikduch <duchpatrik@icloud.com>
2024-03-15 19:03:17 +0100

    PostgresDB connection

--- backendtest/backendtest/settings.py
+++ backendtest/backendtest/settings.py
@@ -75,9 +75,18 @@ WSGI_APPLICATION = 'backendtest.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
