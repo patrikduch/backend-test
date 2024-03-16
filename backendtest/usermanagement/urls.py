a1a12b7703e8a2de03bd205817a4f50ba59e292e
patrikduch <duchpatrik@icloud.com>
2024-03-16 20:29:45 +0100

    Invalidation of Redis Cache

--- backendtest/usermanagement/urls.py
+++ backendtest/usermanagement/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import users_info_view
+from .views import AdminUserCreate
 
 urlpatterns = [
     path('api/users/monthly-signups', users_info_view.as_view(), name='user-info'),
+    path('api/admin/create/', AdminUserCreate.as_view(), name='admin-create'),
 ]
 
--- backendtest/usermanagement/views.py
+++ backendtest/usermanagement/views.py
@@ -2,33 +2,50 @@ from django.core.cache import cache
 from django.db import connection
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import AdminUserSerializer
 
-# Create your views here.
+# Utility function or class method for cache key
+def get_cache_key():
+    return 'user_registrations_by_month'
+
+# Views
 class users_info_view(APIView):
 
+    @staticmethod
+    def get_cache_key():
+        return get_cache_key()
+
     def get(self, request, format=None):
-        # Create a unique cache key based on the endpoint name or path
-        cache_key = 'user_registrations_by_month'
-        # Try to get cached data
+        cache_key = self.get_cache_key()
         cached_data = cache.get(cache_key)
 
-        print(cached_data)
-
         if cached_data is not None:
-            # If cached data is available, use it
             registrations = cached_data
-            print('cached data were used.')
-
         else:
-            # If no cached data, fetch the data from the database
             with connection.cursor() as cursor:
                 cursor.execute("SELECT * FROM get_user_registrations_by_month()")
                 columns = [col[0] for col in cursor.description]
-                registrations = [
-                    dict(zip(columns, row))
-                    for row in cursor.fetchall()
-                ]
-            # Cache the data for next time, set the cache to expire after some time e.g., 1 hour
+                registrations = [dict(zip(columns, row)) for row in cursor.fetchall()]
             cache.set(cache_key, registrations, timeout=3600)
 
         return Response(registrations)
+    
+
+class AdminUserCreate(APIView):
+
+    @staticmethod
+    def get_cache_key():
+        return get_cache_key()
+
+    def post(self, request, format=None):
+        serializer = AdminUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            cache_key = self.get_cache_key()
+            if cache.get(cache_key) is not None:
+                cache.delete(cache_key)
+
+            return Response({"id": user.id, "username": user.username}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
